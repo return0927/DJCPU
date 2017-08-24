07a1d6b4ed5cfa5ecd484c3759cd78819ff532ef
W1nt3r_H4ck <bjleh0927@gmail.com>
2017-08-24 19:36:41 +0900

    Testing Scriptfile.

---
+++ main.c
@@ -0,0 +1,19 @@
+//
+// Created by bjleh on 2017-08-24.
+//
+
+#include <stdio.h>
+
+int main(){
+
+    int input;
+    printf(" Â¦/È¦¼ö ±¸ºĞÇÒ ¼ıÀÚ: ");
+    scanf("%d", &input);
+    if(input%2==0) {
+        printf("\nÂ¦");
+    } else {
+        printf("\nÈ¦");
+    }
+
+    return 0;
+}
